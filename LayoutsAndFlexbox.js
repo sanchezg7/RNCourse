29647a6d5781d408b613b1cdb4c41a44fa100d0e
Gerardo Sanchez <gerardo.sanchez94@gmail.com>
2024-10-25 23:21:43 -0400

    tweaking with flex ratios

--- LayoutsAndFlexbox.js
+++ LayoutsAndFlexbox.js
@@ -8,7 +8,7 @@ export default function LayoutsAndFlexbox() {
                 <TextInput style={styles.textInput} placeholder="Your course goal!"/>
                 <Button title="Add goal" />
             </View>
-            <View>
+            <View style={styles.goalsContainer}>
                 <Text>List of goals...</Text>
             </View>
         </View>
@@ -17,11 +17,18 @@ export default function LayoutsAndFlexbox() {
 
 const styles = StyleSheet.create({
     appContainer: {
-        padding: 50
+        padding: 50,
+        // have to force this container to take all of the height of the viewport
+        flex: 1 // 1 of 1 will take all of it and then child components can divide
     },
     inputContainer: {
+        flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderColor: "#cccccc"
     },
     textInput: {
         borderWidth: 1,
@@ -29,6 +36,10 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding: 5,
         margin: 10,
-        width: "80%"
+        width: "80%",
+    },
+    goalsContainer: {
+        flex: 4
     }
+
 });
